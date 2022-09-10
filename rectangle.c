07c5b85a84ac3144e368a98fb2548e612a9a95af
CharityR <robertscharity47@gmail.com>
2022-09-11 00:19:21 +0100

    arithmetic commit

---
+++ rectangle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+ /* this challenge is to create a C program that displays the perimeter and the area of a rectangle.*/
+
+int main()
+{
+	/* this is to calculate the perimeter of the rectangle*/
+	int a;
+	int b;
+	int c;
+
+	a = height;
+	b = width;
+
+	c = (a + b) * 2;
+
+	printf("Display the value of c : %d \n", c);
+
+	/* this is to calculate the area of the rectangle*/
+
+	int a;
+	int b;
+	int c;
+
+	a = height;
+	b = width;
+
+	c = (a * b);
+
+	printf("Display the value of c : %d \n", c);
+
+	return 0;
+}
